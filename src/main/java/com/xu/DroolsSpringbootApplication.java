3154082c41b382b450faa1b65a3b227617c05537
myherux <heroxu123@gmail.com>
2017-01-06 11:16:00 +0800

    init

---
+++ src/main/java/com/xu/DroolsSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.xu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsSpringbootApplication.class, args);
+	}
+}
